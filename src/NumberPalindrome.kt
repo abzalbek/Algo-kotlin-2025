de67ab1ebf57d0974a0f929b407f89fa606e26dc
Abzal <abzalbekissabekov@Abzals-MacBook-Pro.local>
2021-08-21 14:55:40 -0700

    Add integer palindrome

---
+++ src/NumberPalindrome.kt
@@ -0,0 +1,29 @@
+import org.junit.Test
+
+class NumberPalindrome {
+
+
+    @Test
+    fun testing (){
+
+        val number = 123454321
+        val number2 = 123434210
+
+        println(isIntegerPalindrome(number))
+        println(isIntegerPalindrome(number2))
+
+    }
+
+    fun isIntegerPalindrome(number: Int):Boolean {
+        var check = number
+        var result = 0
+
+        while(check>0){
+            val reminder = check%10
+            result=result*10+reminder
+            check/=10
+        }
+
+        return result==number
+    }
+}
