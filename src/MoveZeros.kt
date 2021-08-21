1c756d9901dd0c2cb35036cfc01d1ad21ced7b09
Abzal <abzalbekissabekov@Abzals-MacBook-Pro.local>
2021-08-21 16:30:02 -0700

    Move zero to left in array

---
+++ src/MoveZeros.kt
@@ -0,0 +1,32 @@
+import org.junit.Test
+
+class MoveZeros {
+
+
+    @Test
+    fun testing() {
+        val arr = intArrayOf(1, 3, 5, 9, 0, 0, 8)
+        arr.forEach { x: Int -> print(x) }
+        println("\n")
+        moveZeros(arr)
+        arr.forEach { x: Int -> print(x) }
+
+    }
+
+    fun moveZeros(a: IntArray): IntArray? {
+        var right = a.size - 1
+        var left = 0
+        while (left < right) {
+            if (a[left] != 0 && a[right] != 0) {
+                right--
+            } else if (a[left] == 0) {
+                left++
+            } else if (a[left] != 0 && a[right] == 0) {
+                val temp = a[left]
+                a[left] = a[right]
+                a[right] = temp
+            }
+        }
+        return a
+    }
+}
