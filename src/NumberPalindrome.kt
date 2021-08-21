ac2bd5a3c29718f9f6f2989763183162594b30c7
Abzal <abzalbekissabekov@Abzals-MacBook-Pro.local>
2021-08-21 14:56:22 -0700

    Fix typo

--- src/NumberPalindrome.kt
+++ src/NumberPalindrome.kt
@@ -4,7 +4,7 @@ class NumberPalindrome {
 
 
     @Test
-    fun testing (){
+    fun testing() {
 
         val number = 123454321
         val number2 = 123434210
@@ -14,16 +14,16 @@ class NumberPalindrome {
 
     }
 
-    fun isIntegerPalindrome(number: Int):Boolean {
+    fun isIntegerPalindrome(number: Int): Boolean {
         var check = number
         var result = 0
 
-        while(check>0){
-            val reminder = check%10
-            result=result*10+reminder
-            check/=10
+        while (check > 0) {
+            val reminder = check % 10
+            result = result * 10 + reminder
+            check /= 10
         }
 
-        return result==number
+        return result == number
     }
 }
