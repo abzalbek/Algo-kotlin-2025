d40abb3ada6a0ff312ffe07805c3fff36a0099be
Abzal <abzalbekissabekov@Abzals-MacBook-Pro.local>
2021-08-30 14:38:12 -0700

    Anagram, reverse array

---
+++ src/Anagram.kt
@@ -0,0 +1,34 @@
+import org.junit.Test
+
+class Anagram {
+
+
+    fun isAnagram(str: String, anagram: String): Boolean {
+        var anagram = anagram
+        if (str.length != anagram.length) {
+            return false
+        }
+
+        for (ch in str.toCharArray()) {
+            if (anagram.indexOf(ch) != -1) {
+                anagram = anagram.substring(0, anagram.indexOf(ch)) + anagram.substring(anagram.indexOf(ch) + 1)
+            } else {
+                return false
+            }
+        }
+        return anagram.isEmpty()
+    }
+
+
+    @Test
+    fun test(){
+        val text1 = "adfg"
+        val text2 = "fagd"
+        val text3 = "fage"
+
+        println(isAnagram(text1,text2))
+        println(isAnagram(text1,text3))
+
+    }
+
+}
---
+++ src/ReverseArray.kt
@@ -0,0 +1,32 @@
+import org.junit.Test
+
+class ReverseArray {
+
+    fun reverse(arr: IntArray): IntArray {
+
+        if (arr.isEmpty()||arr.size<=1) {
+            return arr
+        }
+        var left = 0
+        var right = arr.size - 1
+        while (left < right) {
+            val temp = arr[left]
+            arr[left] = arr[right]
+            arr[right] = temp
+            left++
+            right--
+        }
+        return arr
+    }
+
+    @Test
+    fun test() {
+        val a = intArrayOf(1, 2, 4, 5, 6, 7, 8)
+
+        a.forEach { x: Int -> print(x) }
+        println("\n")
+        reverse(a)
+        a.forEach { x: Int -> print(x) }
+
+    }
+}
